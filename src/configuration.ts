8c08823fef7e3d8499cc30f0c251984e4f552554
Dwayne Charrington <dwaynecharrington@gmail.com>
2021-07-01 20:38:17 +1000

    feat(plugin): import additional items

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,15 @@
 import { DI, IContainer, IRegistry } from '@aurelia/kernel';
 
 import { AureliaTableCustomAttribute } from './aurelia-table-attribute';
+import { AutSortCustomAttribute } from './aurelia-table-sort'; 
+import { AutSelectCustomAttribute } from './aurelia-table-select';
+import { AutPaginationCustomElement } from './aurelia-table-pagination'; 
 
 export const DefaultComponents: IRegistry[] = [
     AureliaTableCustomAttribute as unknown as IRegistry,
+    AutSortCustomAttribute as unknown as IRegistry,
+    AutSelectCustomAttribute as unknown as IRegistry,
+    AutPaginationCustomElement as unknown as IRegistry,
 ];
 
 const aureliaTableConfiguration = {
@@ -20,7 +26,7 @@ const aureliaTableConfiguration = {
 
 export const AureliaTableConfiguration = {
     customize(components: any[] = []) {
-        return { ...cardiganConfiguration };
+        return { ...aureliaTableConfiguration };
     },
     ...aureliaTableConfiguration
 };
